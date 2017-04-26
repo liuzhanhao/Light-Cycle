279a41ce579b2daf0968b7b40e2a0d216c1c3e55
Jasper Lau <jasperlau95@gmail.com>
2017-04-26 21:09:43 +0800

    almost finish part a

--- animate.c
+++ animate.c
@@ -1,5 +1,7 @@
 #include <ncurses.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
 /* Maximum number of keys to read in from the keyboard at once */
 #define MAX_WIDTH	   200
@@ -38,6 +40,7 @@ void assigncolors() {
 	init_pair(LIGHT_COL, COLOR_RED, COLOR_BLACK);
 	init_pair(BOUNDARY_COL, COLOR_WHITE, COLOR_WHITE);
 }
+
 /*
 CYCLE create_cycle(COORDINATE pos, DIRECTION dir) 
 {
@@ -80,10 +83,11 @@ void init_screen()
     clear();
 
     // initialize the cycle
-    COORDINATE pos = {.x = MAX_WIDTH / 2, .y = MAX_HEIGHT - 2};
-	//cycle = create_cycle(pos, EAST);
+    srand(time(NULL));
+    // random initial position
+    COORDINATE pos = {.x = 1 + rand() % (MAX_WIDTH - 2), .y = 1 + rand() % (MAX_HEIGHT - 2)};
 	cycle.pos = pos;
-	cycle.dir = EAST;
+	cycle.dir = rand() % 4;
 	arena[cycle.pos.x][cycle.pos.y] = REDCYCLE;
 }
 
@@ -102,7 +106,7 @@ void draw_map()
         {
             move(y, x);
 
-            if ((x + margin_x >=  MAX_WIDTH) | (y + margin_y >= MAX_HEIGHT))
+            if ((x + margin_x >=  MAX_WIDTH) | (x + margin_x < 0) | (y + margin_y >= MAX_HEIGHT) | (y + margin_y < 0))
             {
             	color_set(NONE_COL, NULL);
                 ch = ' ';
@@ -110,7 +114,7 @@ void draw_map()
             else if (arena[x + margin_x][y + margin_y] == NONE)
             {
             	color_set(NONE_COL, NULL);
-                ch = 'N';
+                ch = ' ';
             }
             else if (arena[x + margin_x][y + margin_y] == REDCYCLE)
             {
@@ -140,33 +144,138 @@ void draw_map()
     refresh();
 }
 
-void move_cycle(CYCLE c)
+// remain_dir = EAST = 1, SOUTH = 2, WEST = 3, NORTH = 4
+int move_cycle(CYCLE* c)
 {
-    DIRECTION dir = c.dir;
+    DIRECTION dir = c->dir;
+    arena[c->pos.x][c->pos.y] = LIGHT;
 
     if (dir == EAST)
     {
-    	arena[c.pos.x + 1][c.pos.y] = REDCYCLE;
-    	arena[c.pos.x][c.pos.y] = LIGHT;
+        if (arena[c->pos.x + 1][c->pos.y] != NONE)
+        {
+            if (arena[c->pos.x][c->pos.y+1] == NONE)
+            {
+                c->dir = SOUTH;
+                return move_cycle(c);
+            }
+            else if (arena[c->pos.x-1][c->pos.y] == NONE)
+            {
+                c->dir = WEST;
+                return move_cycle(c);
+            }
+            else if (arena[c->pos.x][c->pos.y-1] == NONE)
+            {
+                c->dir = NORTH;
+                return move_cycle(c);
+            }
+            return 0;
+        }
+    	arena[++c->pos.x][c->pos.y] = REDCYCLE;
     }
     else if (dir == SOUTH)
     {
-    	arena[c.pos.x][c.pos.y+1] = REDCYCLE;
-    	arena[c.pos.x][c.pos.y] = LIGHT;
+        if (arena[c->pos.x][c->pos.y + 1] != NONE)
+        {
+            if (arena[c->pos.x + 1][c->pos.y] == NONE)
+            {
+                c->dir = EAST;
+                return move_cycle(c);
+            }
+            else if (arena[c->pos.x-1][c->pos.y] == NONE)
+            {
+                c->dir = WEST;
+                return move_cycle(c);
+            }
+            else if (arena[c->pos.x][c->pos.y-1] == NONE)
+            {
+                c->dir = NORTH;
+                return move_cycle(c);
+            }
+            return 0;
+        }
+    	arena[c->pos.x][++c->pos.y] = REDCYCLE;
     }
     else if (dir == WEST)
     {
-    	arena[c.pos.x-1][c.pos.y] = REDCYCLE;
-    	arena[c.pos.x][c.pos.y] = LIGHT;
+        if (arena[c->pos.x - 1][c->pos.y] != NONE)
+        {
+            if (arena[c->pos.x][c->pos.y+1] == NONE)
+            {
+                c->dir = SOUTH;
+                return move_cycle(c);
+            }
+            else if (arena[c->pos.x + 1][c->pos.y] == NONE)
+            {
+                c->dir = EAST;
+                return move_cycle(c);
+            }
+            else if (arena[c->pos.x][c->pos.y-1] == NONE)
+            {
+                c->dir = NORTH;
+                return move_cycle(c);
+            }
+            return 0;
+        }
+    	arena[--c->pos.x][c->pos.y] = REDCYCLE;
     }
     else if (dir == NORTH)
     {
-    	arena[c.pos.x][c.pos.y-1] = REDCYCLE;
-    	arena[c.pos.x][c.pos.y] = LIGHT;
+        if (arena[c->pos.x][c->pos.y - 1] != NONE)
+        {
+            if (arena[c->pos.x][c->pos.y+1] == NONE)
+            {
+                c->dir = SOUTH;
+                return move_cycle(c);
+            }
+            else if (arena[c->pos.x-1][c->pos.y] == NONE)
+            {
+                c->dir = WEST;
+                return move_cycle(c);
+            }
+            else if (arena[c->pos.x + 1][c->pos.y] == NONE)
+            {
+                c->dir = EAST;
+                return move_cycle(c);
+            }
+            return 0;
+        }
+    	arena[c->pos.x][--c->pos.y] = REDCYCLE;
     }
 
-    //clear();
+    clear();
    	draw_map();
+    return 1;
+}
+
+// check if the cycle can move to that direction
+bool check(CYCLE c, DIRECTION dir)
+{
+    if (dir == EAST)
+        return arena[c.pos.x+1][c.pos.y] == NONE;
+    else if (dir == SOUTH)
+        return arena[c.pos.x][c.pos.y+1] == NONE;
+    else if (dir == WEST)
+        return arena[c.pos.x-1][c.pos.y] == NONE;
+    else if (dir == NORTH)
+        return arena[c.pos.x][c.pos.y-1] == NONE;
+    return false;
+}
+
+void pick_an_direction(CYCLE *c)
+{
+    int x = c->pos.x, y = c->pos.y;
+    // if all direction is unreachable, break
+    if (arena[x+1][y] != NONE || arena[x][y+1] != NONE || arena[x-1][y] != NONE || arena[x][y] != NONE)
+    {
+        DIRECTION old_dir = c->dir;
+        DIRECTION new_dir = rand() % 4;
+        while (new_dir == old_dir || !check(cycle, new_dir))
+        {
+            new_dir = rand() % 4;
+        }
+        c->dir = new_dir;
+    }
 }
 
 
@@ -175,11 +284,18 @@ int main(int argc, char *argv[])
 
 	init_screen();
 	draw_map();
-	sleep(2);
-	move_cycle(cycle);
+    int flag = 1;
+    while(flag)
+    {
+        usleep(50000);
+        flag =  move_cycle(&cycle);
+        //judge if next position is a wall
+        if (rand() % 10 == 0)
+            pick_an_direction(&cycle);
+    }
 
-	sleep(10);
+	sleep(3);
 	endwin();
-
+    printf("Game Over!\n");
 	return 0;
 }
