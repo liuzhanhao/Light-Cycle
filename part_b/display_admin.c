7421fc2d1c7714431c4cc196c8238e4543fe12c7
Jasper Lau <jasperlau95@gmail.com>
2017-04-30 02:45:49 +0800

    dsaf

--- part_b/display_admin.c
+++ part_b/display_admin.c
@@ -8,49 +8,91 @@
 
 int main(int argc, char* argv[]) {
 
-    char *fromWhom = NULL;
-    MESSAGE msg, reply;
+    char *fromWhom1 = NULL;
+    char *fromWhom2 = NULL;
+    MESSAGE msg1, msg2, reply1, reply2;
+    MESSAGE fifo[20];
+    int fifo_size = 0, fi = 0;
+
+    FILE *f = fopen("file.txt", "w"); // for test
+    fprintf(f, "1st line!\n");
+
 
     if (name_attach("Display_Admin", NULL) == -1) {
-        fprintf(stderr, "Cannot attach name!\n");
+        fprintf(stderr, "Cannot attach name in display_admin.c!\n");
         exit(0);
     }
 
-    while (lowerBound < upperBound) {
-        /* Receive a guess from a worker */
-        if (Receive(&fromWhom, &msg, sizeof(msg)) == -1) {
-            fprintf(stderr, "Cannot receive message!\n");
+    while (msg2.type != END) {
+
+        if (Receive(&fromWhom1, &msg1, sizeof(msg1)) == -1) {
+            fprintf(stderr, "Cannot receive message from painter!\n");
             exit(0);
         }
 
-        /* Adjust the bounds
-         * When guess == target, then lowerBound == upperBound
-         * */
-        if (msg.guess >= lowerBound && msg.guess <= upperBound) {
-            if (msg.guess == target) {
-                upperBound = target;
-                lowerBound = target;
-            } else if (msg.guess > target) {
-                upperBound = msg.guess - 1;
-            } else if (msg.guess < target) {
-                lowerBound = msg.guess + 1;
+        if (Receive(&fromWhom2, &msg2, sizeof(msg2)) == -1) {
+            fprintf(stderr, "Cannot receive message from courier!\n");
+            exit(0);
             }
-            printf("Bounds = [%d, %d]\n", lowerBound, upperBound);
+
+        printf("%s\n", fromWhom2); // painter, bug
+
+        if (msg2.type == DISPLAY_ARENA){
+            fprintf(f, "DISPLAY_ARENA!\n");
+            fifo[(fi + fifo_size) % 20] = msg2;
+            fifo_size++;
+
+            reply2.type = OKAY;
+            if (Reply(fromWhom2, &reply2, sizeof(reply2)) == -1) {
+                fprintf(f, "Cannot reply message!\n");
+                exit(0);
+            }
+            fprintf(f, "replied OKAY!\n");
         }
 
-        /* Return an updated bounds to that worker */
-        reply.lowerBound = lowerBound;
-        reply.upperBound = upperBound;
-        if (Reply(fromWhom, &reply, sizeof(reply)) == -1) {
-            fprintf(stderr, "Cannot reply message!\n");
-            exit(0);
+        if (msg2.type == END){
+            fprintf(f, "end!\n");
+            fclose(f);
+            reply1.type = END;
+            reply1.arena = msg2.arena;
+            reply1.cycleId = msg2.cycleId;
+            if (Reply(fromWhom1, &reply1, sizeof(reply1)) == -1) {
+                fprintf(stderr, "Cannot reply message!\n");
+                exit(0);
+            }
+
+            reply2.type = OKAY;
+            if (Reply(fromWhom2, &reply2, sizeof(reply2)) == -1) {
+                fprintf(stderr, "Cannot reply message!\n");
+                exit(0);
+            }
+            break;
+        }
+
+        if (msg1.type == PAINTER_READY){
+
+            reply1.type = PAINT;
+            reply1.arena = fifo[fi].arena;
+            if (fi < 19) fi++;
+            else if (fi == 19) fi = 0;
+            fifo_size--;
+
+            if (Reply(fromWhom1, &reply1, sizeof(reply1)) == -1) {
+                fprintf(stderr, "Cannot reply message!\n");
+                exit(0);
+            }
+            fprintf(f, "replied PAINT!\n");
         }
     }
+
+    //fprintf(f, "break!\n");
+    
 
     if (name_detach() == -1) {
         fprintf(stderr, "Cannot detach name!\n");
         exit(0);
     }
 
+    
     return 0;
 }
