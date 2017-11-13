c72e8e1a535dee01ed37316bad0ae3f3aaf71faa
Zhanhao (Jasper) Liu <zhliu6@cse.cuhk.edu.hk>
2017-11-14 00:54:02 +0800

    update

--- part_b/keyboard.c
+++ part_b/keyboard.c
@@ -1,21 +1,3 @@
-/*
-* CSCI3180 Principles of Programming Languages
-*
-* --- Declaration ---
-*
-* I declare that the assignment here submitted is original except for source
-* material explicitly acknowledged. I also acknowledge that I am aware of
-* University policy and regulations on honesty in academic work, and of the
-* disciplinary guidelines and procedures applicable to breaches of such policy
-* and regulations, as contained in the website
-* http://www.cuhk.edu.hk/policy/academichonesty/
-*
-* Assignment 5A
-* Name : Liu Zhan Hao
-* Student ID : 1155092201
-* Email Addr : zhliu6@cse.cuhk.edu.hk
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
